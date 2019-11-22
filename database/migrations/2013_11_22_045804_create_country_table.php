39384e7962b94d6f3e7e1c698052d8dc4c8742b1
Rajan <rajan@freshbits.in>
2019-11-22 10:41:08 +0530

    user & country table migrate with relation
    Signed-off-by: Rajan <rajan@freshbits.in>

--- database/migrations/2013_11_22_045804_create_country_table.php
+++ database/migrations/2013_11_22_045804_create_country_table.php
@@ -14,7 +14,8 @@ class CreateCountryTable extends Migration
     public function up()
     {
         Schema::create('countries', function (Blueprint $table) {
-
+            $table->increments('id');
+            $table->string('name');
         });
     }
 
